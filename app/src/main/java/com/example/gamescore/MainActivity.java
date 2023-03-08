7f19081b8f79c4b0520b2c83386bae4593fa762a
Alexey Zagrebin <azagrebin244@gmail.com>
2023-03-08 13:59:18 +0300

    fixed zeroing values ​​when flipping the device

--- app/src/main/java/com/example/gamescore/MainActivity.java
+++ app/src/main/java/com/example/gamescore/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gamescore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,18 +9,24 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int score1 = 0;
-    private static int score2 = 0;
+    private int score1 = 0;
+    private int score2 = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null){
+            score1 = savedInstanceState.getInt("score1");
+            score2 = savedInstanceState.getInt("score2");
+        }
+
         TextView textView3ClickTeam1 = findViewById(R.id.textView3ClickTeam1);
         TextView textView4ClickTeam2 = findViewById(R.id.textView4ClickTeam2);
 
-
+        textView3ClickTeam1.setText(String.valueOf(score1));
+        textView4ClickTeam2.setText(String.valueOf(score2));
 
         textView3ClickTeam1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,6 +41,17 @@ public class MainActivity extends AppCompatActivity {
                 textView4ClickTeam2.setText(String.valueOf(++score2));
             }
         });
+    }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("score1", score1);
+        outState.putInt("score2", score2);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
